e61be61e35636119e94fc6a5c6be9f0df34855b5
Richard Liu <richardadesigner@gmail.com>
2026-01-04 13:04:40 -0800

    update

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 function getFactorial(input) {
-    let result = 1;
-    let n = input;
-    while (n > 1) {
-        result = result * n;
-        n--
+    if (input < 0) {
+        throw new Error("input must be larger or equal to 0")
+    } else if (input != Math.round(input)) {
+        throw new Error("input must be an integer")
+    } else {
+        let result = 1;
+        let n = input;
+        while (n > 1) {
+            result = result * n;
+            n--
+        }
+        return result;
     }
-    return result;
 }
